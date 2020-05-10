9053675bf55069597293411a258192bb113882e6
unknown <moloko3@ukr.net>
2020-05-10 16:51:44 +0300

    add

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5001
 
 // midleware
 app.use(cors())
